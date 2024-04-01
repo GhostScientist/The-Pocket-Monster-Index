ef5b880007026326751810d75fdd63389242aaa1
Dakota Kim <codingvampire@gmail.com>
2024-04-01 01:20:56 -0400

    Initial Commit

---
+++ Pocket Monster Index Watch App/Pocket_Monster_IndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_Monster_IndexApp.swift
+//  Pocket Monster Index Watch App
+//
+//  Created by Dakota Kim on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_Monster_Index_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
